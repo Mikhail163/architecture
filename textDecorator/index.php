f6b278f89a8001a27c7ea4523b3ebe34a7570a83
Mikhail <mephist0_87@mail.ru>
2019-01-15 07:32:18 +0000

    textDecorator

---
+++ textDecorator/index.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once 'iText.php';
+require_once 'TextDecorator.php';
+require_once 'Text.php';
+require_once 'DangerousHTMLTagsFilter.php';
+require_once 'MarkdownFormat.php';
+require_once 'PlainTextFilter.php';
+
+use Decorator\TextFormat\iText;
+use Decorator\TextFormat\Text;
+use Decorator\TextFormat\DangerousHTMLTagsFilter;
+use Decorator\TextFormat\MarkdownFormat;
+use Decorator\TextFormat\PlainTextFilter;
+
+/**
+ * Клиентский код может быть частью реального веб-сайта, который отображает
+ * создаваемый пользователями контент. Поскольку он работает с модулями
+ * форматирования через интерфейс компонента, ему всё равно, получает ли он
+ * простой объект компонента или обёрнутый.
+ */
+function displayCommentAsAWebsite(iText $format, string $text)
+{
+	echo $format->formatText($text);
+}
+
+/**
+ * Модули форматирования пользовательского ввода очень удобны при работе с
+ * контентом, создаваемым пользователями. Отображение такого контента «как есть»
+ * может быть очень опасным, особенно когда его могут создавать анонимные
+ * пользователи (например, комментарии). Ваш сайт не только рискует получить
+ * массу спам-ссылок, но также может быть подвергнут XSS-атакам.
+ */
+$dangerousComment = <<<HERE
+Hello! Nice blog post!
+Please visit my <a href='http://www.iwillhackyou.com'>homepage</a>.
+<script src="http://www.iwillhackyou.com/script.js">
+  performXSSAttack();
+</script>
+HERE;
+
+/**
+ * Наивное отображение комментариев (небезопасное).
+ */
+$naiveInput = new Text;
+echo "Website renders comments without filtering (unsafe):\n";
+displayCommentAsAWebsite($naiveInput, $dangerousComment);
+echo "\n\n\n";
+
+/**
+ * Отфильтрованное отображение комментариев (безопасное).
+ */
+$filteredInput = new PlainTextFilter($naiveInput);
+echo "Website renders comments after stripping all tags (safe):\n";
+displayCommentAsAWebsite($filteredInput, $dangerousComment);
+echo "\n\n\n";
+
+
+/**
+ * Декоратор позволяет складывать несколько входных форматов для получения
+ * точного контроля над отображаемым содержимым.
+ */
+$dangerousForumPost = <<<HERE
+# Welcome
+		
+This is my first post on this **gorgeous** forum.
+		
+<script src="http://www.iwillhackyou.com/script.js">
+  performXSSAttack();
+</script>
+HERE;
+
+/**
+ * Наивное отображение сообщений (небезопасное, без форматирования).
+ */
+$naiveInput = new Text;
+echo "Website renders a forum post without filtering and formatting (unsafe, ugly):\n";
+displayCommentAsAWebsite($naiveInput, $dangerousForumPost);
+echo "\n\n\n";
+
+/**
+ * Форматтер Markdown + фильтрация опасных тегов (безопасно, красиво).
+ */
+$text = new Text;
+$markdown = new MarkdownFormat($text);
+$filteredInput = new DangerousHTMLTagsFilter($markdown);
+echo "Website renders a forum post after translating markdown markup" .
+		"and filtering some dangerous HTML tags and attributes (safe, pretty):\n";
+displayCommentAsAWebsite($filteredInput, $dangerousForumPost);
+echo "\n\n\n";
